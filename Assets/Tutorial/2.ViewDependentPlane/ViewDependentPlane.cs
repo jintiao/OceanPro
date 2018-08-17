dbe99573cf24711154bb52065ea7dcf1e21309c0
jintiao <jintiao@outlook.com>
2018-08-18 02:38:24 +0800

    ViewDependentPlane

---
+++ Assets/Tutorial/2.ViewDependentPlane/ViewDependentPlane.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OceanPro
+{
+	[ExecuteInEditMode]
+	public class ViewDependentPlane : MonoBehaviour
+	{
+		private MeshFilter meshFilter;
+		private MeshRenderer meshRenderer;
+
+		public int gridSizeX = 20;
+		public int gridSizeZ= 20;
+
+		public float planeY = 0;
+		public float planeWidth = 100;
+
+		private Material material;
+		private int planeParamId;
+
+		private Vector3[] vertices;
+
+		private void Start()
+		{
+			if(!meshFilter)
+			{
+				meshFilter = GetComponent<MeshFilter>();
+
+				if(!meshFilter)
+				{
+					meshFilter = gameObject.AddComponent<MeshFilter>();
+				}
+			}
+
+			if(!meshRenderer)
+			{
+				meshRenderer = GetComponent<MeshRenderer>();
+
+				if(!meshRenderer)
+				{
+					meshRenderer = gameObject.AddComponent<MeshRenderer>();
+				}
+			}
+
+			meshFilter.mesh = CreateMesh();
+
+			planeParamId = Shader.PropertyToID("_PlaneParam");
+		}
+
+		private void Update()
+		{
+			
+			if (material == null)
+				material = meshRenderer.sharedMaterial;
+			
+			if (material != null)
+			{
+				var param = new Vector4();
+				param.x = Camera.main.transform.position.x;
+				param.y = planeY;
+				param.z = Camera.main.transform.position.z;
+				param.w = planeWidth;
+
+				material.SetVector(planeParamId, param);
+
+				gameObject.transform.position = new Vector3(param.x, param.y, param.z);
+				meshFilter.sharedMesh.bounds = new Bounds(new Vector3(0.5f, 0, 0.5f), new Vector3(planeWidth, 1, planeWidth));
+			}
+		}
+
+		private Mesh CreateMesh()
+		{
+			int scrGridSizeX = gridSizeX;
+			int scrGridSizeZ = gridSizeZ;
+
+			float rcpScrGridSizeX = 1.0f / (scrGridSizeX - 1.0f);
+			float rcpScrGridSizeZ = 1.0f / (scrGridSizeZ - 1.0f);
+
+			int verticesCount = scrGridSizeX * scrGridSizeZ;
+			vertices = new Vector3[verticesCount];
+
+			int count = 0;
+			for(int z = 0; z < scrGridSizeZ; z++)
+			{
+				var vvz = z * rcpScrGridSizeZ;
+
+				for(int x = 0; x < scrGridSizeX; x++)
+				{
+					var pos = new Vector3();
+					pos.x = x * rcpScrGridSizeX;
+					pos.y = 0;
+					pos.z = vvz;
+					vertices[count] = pos;
+
+					count++;
+				}
+			}
+
+			int indicesCount = (scrGridSizeX - 1) * (scrGridSizeZ - 1) * 6;
+			var indices = new int[indicesCount];
+
+			count = 0;
+			for(int z = 0; z < scrGridSizeZ - 1; z++)
+			{
+				for(int x = 0; x < scrGridSizeX - 1; x++)
+				{
+					var n = z * scrGridSizeX + x;
+
+					indices[count] = n;
+					indices[count + 1] = n + scrGridSizeX;
+					indices[count + 2] = n + 1;
+
+					indices[count + 3] = n + scrGridSizeX;
+					indices[count + 4] = n + scrGridSizeX + 1;
+					indices[count + 5] = n + 1;
+
+					count += 6;
+				}
+			}
+
+			var mesh = new Mesh();
+			mesh.name = "ProcedualMesh";
+			mesh.vertices = vertices;
+			mesh.triangles = indices;
+
+			return mesh;
+		}
+	}
+}
---
+++ Assets/Tutorial/2.ViewDependentPlane/ViewDependentPlaneShader.shader
@@ -0,0 +1,54 @@
+﻿Shader "Unlit/OceanPro/Tutorial/ViewDependentPlane"
+{
+	Properties
+	{
+		_PlaneParam ("Plane Params", Vector) = (0,0,0,0) 
+	}
+
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			float4 _PlaneParam;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				UNITY_INITIALIZE_OUTPUT(v2f, o);
+
+				float3 wpos = 0;
+				wpos.xz = (v.vertex.xz * 2 - 1) * _PlaneParam.w + _PlaneParam.xz;
+				wpos.y = _PlaneParam.y;
+
+				o.vertex = UnityWorldToClipPos(wpos);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				return fixed4(0.7, 0.0, 0.0, 1);
+			}
+			ENDCG
+		}
+	}
+}
